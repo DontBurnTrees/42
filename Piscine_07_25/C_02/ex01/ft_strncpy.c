8d0909e950f7495a5e0e882753663aab15b76fdb
DontBurnTrees <141053010+DontBurnTrees@users.noreply.github.com>
2025-07-21 10:57:03 +0200

    Create ft_strncpy.c

---
+++ Piscine_07_25/C_02/ex01/ft_strncpy.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+char    *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i;
+    
+    i = 0;
+    while (i < n && src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+    return dest;
+}
+
+/*int main(void)
+{
+    char src[] = "thesource";
+    char dest[100];
+
+    printf("La source: %s, La destination: %s\n", src, dest);
+    ft_strncpy(dest, src, 4);
+    printf("La source: %s, La destination: %s\n", src, dest);
+}*/
