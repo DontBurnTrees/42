c62dcaaa53e51a284c86d2ca5f1b833da3fb64b0
DontBurnTrees <141053010+DontBurnTrees@users.noreply.github.com>
2025-07-24 09:36:23 +0200

    Update ft_str_is_alpha.c
    adding code

--- Piscine_07_25/C_02/ex02/ft_str_is_alpha.c
+++ Piscine_07_25/C_02/ex02/ft_str_is_alpha.c
@@ -1,1 +1,26 @@
-aa
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	a;
+
+	a = 0;
+	while (str[a] != '\0')
+	{
+		if (!((str[a] >= 'a' && str[a] <= 'z') ||
+		(str[a] >= 'A' && str[a] <= 'Z'))) 
+		{
+			return (0);
+		}
+		a++;
+	}
+	return (1);
+}
+
+/*int	main(void)
+{
+	printf("%d\n", ft_str_is_alpha("phrase"));
+	printf("%d\n", ft_str_is_alpha("PHRASE"));
+	printf("%d\n", ft_str_is_alpha("phr4se"));
+	return (0);
+}*/
