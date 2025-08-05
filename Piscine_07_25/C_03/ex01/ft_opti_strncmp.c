184b35dde4f5d2596fa6427b5188e3d1460b02cc
DontBurnTrees <141053010+DontBurnTrees@users.noreply.github.com>
2025-08-05 10:49:17 +0200

    Create ft_opti_strncmp.c

---
+++ Piscine_07_25/C_03/ex01/ft_opti_strncmp.c
@@ -0,0 +1,15 @@
+int     ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+        unsigned int    count;
+
+        count = 0;
+        while (count < n && *s1 && *s1 == *s1)
+        {
+                count++;
+                s1++;
+                s2++;
+        }
+        if (count == n)
+                return (0);
+        return ((unsigned char)*s1 - (unsigned char)*s2);
+}
