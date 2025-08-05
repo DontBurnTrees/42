69ebefe9f6ff1f65b3e39d436e8d698816e60ebc
DontBurnTrees <141053010+DontBurnTrees@users.noreply.github.com>
2025-08-05 10:23:18 +0200

    Add files via upload

---
+++ Piscine_07_25/C_03/ex01/ft_strncmp.c
@@ -0,0 +1,20 @@
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && s1[i] && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+/*int	main(void)
+{
+	char	*s1 = "Pierre";
+	char	*s2 = "Pietre";
+
+	printf("%d\n", ft_strncmp(s1, s2, 4));
+	return (0);
+}*/
