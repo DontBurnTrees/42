c481d55e3e6b866c5f245040bc5cfa11ec07a8f1
DontBurnTrees <141053010+DontBurnTrees@users.noreply.github.com>
2025-08-21 11:43:09 +0200

    Update ft_rev_int_tab.c

--- Piscine_07_25/C01/ex07/ft_rev_int_tab.c
+++ Piscine_07_25/C01/ex07/ft_rev_int_tab.c
@@ -1,42 +1,15 @@
-#include <stdio.h>
-
-void	ft_swap(int *a, int *b){
-	int	temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
 void	ft_rev_int_tab(int *tab, int size)
 {
 	int	i = 0;
 	int	j = size - 1;
+	int 	tmp;
 
 	while (i < j)
 	{
-		ft_swap(&tab[i], &tab[j]);
+		tmp = tab[i];
+		tab[i] = tab[j];
+		tab[j] = tmp;
 		i++;
 		j--;
 	}
 }
-
-void    print_tab(int *tab, int size)
-{
-    int i = 0;
-
-    while (i < size)
-    {
-        printf("%d ", tab[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/*int	main(void)
-{
-	int	tab[5] = {1, 2, 3, 4, 5};
-
-	ft_rev_int_tab(tab, 5);
-	print_tab(tab, 5);
-}*/
